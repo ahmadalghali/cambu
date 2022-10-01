5dc059983e027b8595bae71f39c8373faa306df6
Ahmad Alghali <a.alghali@primeboard.org>
2022-10-01 22:05:52 +0100

    add hero

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts"></script>
 <template>
   <div class="hero"></div>
-  <!-- <div
+  <div
     class="hero min-h-screen"
     style="
       background-image: url(https://images.unsplash.com/photo-1549298916-b41d501d3772?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2912&q=80);
@@ -19,6 +19,6 @@
         <button class="btn btn-primary">Get Started</button>
       </div>
     </div>
-  </div> -->
+  </div>
 </template>
 <style scoped></style>
