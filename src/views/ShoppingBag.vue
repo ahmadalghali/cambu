74e8d5ae893240897b2109736a33006327979b07
Ahmad Alghali <a.alghali@primeboard.org>
2022-10-06 05:55:41 +0100

    responsive shopping bag page

--- src/views/ShoppingBag.vue
+++ src/views/ShoppingBag.vue
@@ -23,45 +23,89 @@ const subtotal = ref(0);
       </div>
 
       <p class="mt-4 font-medium text-lg">£{{ total }}</p>
-      <hr class="my-5" />
+      <hr class="my-5 xl:hidden" />
 
-      <div class="items my-10 space-y-10">
-        <div v-for="(item, index) in bagItems" :key="index" class="">
-          <BagItem :item="item" />
-          <hr class="mt-5" />
+      <div class="hidden xl:grid grid-cols-2 gap-x-5 mt-5">
+        <div class="space-y-10">
+          <div v-for="(item, index) in bagItems" :key="index" class="">
+            <BagItem :item="item" />
+            <hr class="mt-5" />
+          </div>
+        </div>
+
+        <div class="">
+          <div class="">
+            <p class="text-lg">Have a voucher code?</p>
+
+            <div class="flex space-x-3 mt-1">
+              <input
+                type="text"
+                class="border border-black rounded-md w-full px-3 text-lg uppercase font-medium focus:outline-black"
+              />
+              <button class="btn btn-primary rounded-full px-6">Apply</button>
+            </div>
+          </div>
+
+          <div class="mt-10 text-lg space-y-3">
+            <div class="flex justify-between">
+              <p>Subtotal</p>
+              <p>£{{ subtotal }}</p>
+            </div>
+            <div class="flex justify-between">
+              <p>Delivery</p>
+              <p>£{{ deliveryCost }}</p>
+            </div>
+            <div class="flex justify-between">
+              <p>Total</p>
+              <p>£{{ total }}</p>
+            </div>
+          </div>
+
+          <button class="btn btn-primary rounded-full w-full mt-10 h-14">
+            Checkout
+          </button>
         </div>
       </div>
 
-      <div class="">
-        <p class="text-lg">Have a voucher code?</p>
-
-        <div class="flex space-x-3 mt-1">
-          <input
-            type="text"
-            class="border border-black rounded-md w-full px-3 text-lg uppercase font-medium focus:outline-black"
-          />
-          <button class="btn btn-primary rounded-full px-6">Apply</button>
+      <div class="mobile xl:hidden">
+        <div class="items my-10 space-y-10">
+          <div v-for="(item, index) in bagItems" :key="index" class="">
+            <BagItem :item="item" />
+            <hr class="mt-5" />
+          </div>
         </div>
+
+        <div class="">
+          <p class="text-lg">Have a voucher code?</p>
+
+          <div class="flex space-x-3 mt-1">
+            <input
+              type="text"
+              class="border border-black rounded-md w-full px-3 text-lg uppercase font-medium focus:outline-black"
+            />
+            <button class="btn btn-primary rounded-full px-6">Apply</button>
+          </div>
+        </div>
+
+        <div class="mt-10 text-lg space-y-3">
+          <div class="flex justify-between">
+            <p>Subtotal</p>
+            <p>£{{ subtotal }}</p>
+          </div>
+          <div class="flex justify-between">
+            <p>Delivery</p>
+            <p>£{{ deliveryCost }}</p>
+          </div>
+          <div class="flex justify-between">
+            <p>Total</p>
+            <p>£{{ total }}</p>
+          </div>
+        </div>
+
+        <button class="btn btn-primary rounded-full w-full mt-10 h-14">
+          Checkout
+        </button>
       </div>
-
-      <div class="mt-10 text-lg space-y-3">
-        <div class="flex justify-between">
-          <p>Subtotal</p>
-          <p>£{{ subtotal }}</p>
-        </div>
-        <div class="flex justify-between">
-          <p>Delivery</p>
-          <p>£{{ deliveryCost }}</p>
-        </div>
-        <div class="flex justify-between">
-          <p>Total</p>
-          <p>£{{ total }}</p>
-        </div>
-      </div>
-
-      <button class="btn btn-primary rounded-full w-full mt-10 h-14">
-        Checkout
-      </button>
     </div>
     <div v-else class="grid place-items-center gap-y-10 my-40">
       <svg
