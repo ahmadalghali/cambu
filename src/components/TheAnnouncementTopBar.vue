ce3f64691c4e15effb7523062c042657228f7842
Ahmad Alghali <ahmad-mustafaalghali@hotmail.com>
2024-02-13 09:37:37 +0000

    fix typo

--- src/components/TheAnnouncementTopBar.vue
+++ src/components/TheAnnouncementTopBar.vue
@@ -4,7 +4,7 @@
     id="announcement-bar"
     class="bg-primary fixed left-0 right-0 top-0 text-white py-3 text-center text-sm font-medium z-50"
   >
-    <p>Free Shipping on orders eligible orders above £50</p>
+    <p>Free Shipping on eligible orders above £50</p>
   </div>
 </template>
 <style scoped></style>
